1a603f03567185c83a28dd0fd89f869cd7c42ac8
sgoenko <sgoenko@gmail.com>
2021-08-04 16:48:26 +0300

    Init

---
+++ src/test/java/com/polymir/Calibration/CalibrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polymir.Calibration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalibrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
